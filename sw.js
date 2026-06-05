7f2c3a4ad3e4e8f7808534b236d3134a7de6a9db
zz-1-z10 <2947983055@qq.com>
2026-06-05 23:51:18 +0800

    sw缓存v4优先网络

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-const CACHE = 'photo-accounting-v1';
-const FILES = ['index.html', 'manifest.json', 'icon.svg', 'icon-192.png', 'icon-512.png'];
+const CACHE = 'photo-accounting-v4';
+const FILES = ['manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(FILES))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
